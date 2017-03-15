65dc7929ba17c38f12ce7c1f13393f2ff097197a
Nataliya Georgieva <ngeorgieva@axway.com>
2017-03-15 12:39:39 +0200

    Ran standard through connect & disconnect unit test files

--- test/unit/lifecycle/disconnect.js
+++ test/unit/lifecycle/disconnect.js
@@ -1,45 +1,43 @@
-const test = require('tap').test;
-const sinon = require('sinon');
-const disconnectMethod = require('../../../lib/lifecycle/disconnect')['disconnect'];
+const test = require('tap').test
+const sinon = require('sinon')
+const disconnectMethod = require('../../../lib/lifecycle/disconnect')['disconnect']
 
 test('### Test Disconnect method with connection ###', sinon.test(function (t) {
-    
     // Data
-    const context = {};
-    context.connection = {
-        close : function () { }
-    };
+  const context = {}
+  context.connection = {
+    close: function () { }
+  }
 
     // Stubs & spies
-    function next() { };
-    const nextSpy = this.spy(next);
+  function next () { };
+  const nextSpy = this.spy(next)
 
     // Execution
-    disconnectMethod.bind(context, nextSpy)();
+  disconnectMethod.bind(context, nextSpy)()
 
     // Test
-    t.ok(nextSpy.calledOnce);
-    t.ok(nextSpy.args[0].length === 0);
-    t.ok(context.connection === null);
-    
-    t.end();
+  t.ok(nextSpy.calledOnce)
+  t.ok(nextSpy.args[0].length === 0)
+  t.ok(context.connection === null)
+
+  t.end()
 }))
 
 test('### Test Disconnect method no connection ###', sinon.test(function (t) {
-    
     // Data
-    const context = {};
+  const context = {}
 
     // Stubs & spies
-    function next() { };
-    const nextSpy = this.spy(next);
+  function next () { }
+  const nextSpy = this.spy(next)
 
     // Execution
-    disconnectMethod.bind(context, nextSpy)();
+  disconnectMethod.bind(context, nextSpy)()
 
     // Test
-    t.ok(nextSpy.calledOnce);
-    t.ok(nextSpy.args[0].length === 0);
-    
-    t.end();
+  t.ok(nextSpy.calledOnce)
+  t.ok(nextSpy.args[0].length === 0)
+
+  t.end()
 }))
